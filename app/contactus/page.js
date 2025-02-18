e811ba3571366be2a4caff9acae1a95a00488054
Imtiaz Laptop <imteiaz@gmail.com>
2025-02-18 00:41:19 +0000

    Dot added in contact us page

--- app/contactus/page.js
+++ app/contactus/page.js
@@ -7,7 +7,7 @@ const ContactUs = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Contact Us
+              Contact Us.
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
               Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
