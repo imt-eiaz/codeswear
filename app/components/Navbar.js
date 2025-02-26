9f061e22337c32b14bc55b7fec277886dd1ecbff
Imtiaz Laptop <imteiaz@gmail.com>
2025-02-26 08:53:01 +0000

    Start from video 36 ts 19:36 in Next Js Codewithharry

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,48 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <header className="text-gray-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span className="ml-3 text-xl">Tailblocks</span>
-          </a>
-          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-gray-900">First Link</a>
-            <a className="mr-5 hover:text-gray-900">Second Link</a>
-            <a className="mr-5 hover:text-gray-900">Third Link</a>
-            <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-          </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+    <>
+      <div className="flex flex-col md:flex-row md:justify-start justify-center items-center">
+        <div className="logo mx-5">
+          <Image width={100} height={40} src="/logo.png" alt="" />
         </div>
-      </header>
-    </div>
+        <div className="nav">
+          <ul className="flex items-center space-x-2 font-bold md:text-xl">
+            <Link href={"/"} legacyBehavior>
+              <a>
+                <li>Tshirts</li>
+              </a>
+            </Link>
+            <Link href={"/"} legacyBehavior>
+              <a>
+                <li>Hoodies</li>
+              </a>
+            </Link>
+            <Link href={"/"} legacyBehavior>
+              <a>
+                <li>Stickers</li>
+              </a>
+            </Link>
+            <Link href={"/"} legacyBehavior>
+              <a>
+                <li>Mugs</li>
+              </a>
+            </Link>
+          </ul>
+        </div>
+        <div className="cart absolute right-0 top-2 mx-5">
+          <button>Cart</button>
+        </div>
+      </div>
+    </>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -2,14 +2,12 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import home from "../public/home.png";
 import Image from "next/image";
-import Hero from "./components/Hero";
 import Content from "./components/Content";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
       <div>
         <Image
           src={home}
