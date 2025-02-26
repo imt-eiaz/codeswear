a690ec9c774e3c50e86556ba4fce6aa9fce5e17a
Imtiaz Laptop <imteiaz@gmail.com>
2025-02-26 22:48:41 +0000

    End of video 36

--- app/components/Content.js
+++ app/components/Content.js
@@ -7,11 +7,16 @@ const Content = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-              Pitchfork Kickstarter Taxidermy
+              Wear the code with wearthecode.com
             </h1>
             <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table.
+              Wear whatever what ever you want you want ? waearthe code.
+            </p>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+              Wear whatever what ever you want you want ? waearthe code.
+            </p>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+              Wear whatever what ever you want you want ? waearthe code.
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { IoCartOutline } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row md:justify-start justify-center items-center">
+      <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2">
         <div className="logo mx-5">
           <Image width={100} height={40} src="/logo.png" alt="" />
         </div>
@@ -34,7 +35,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="cart absolute right-0 top-2 mx-5">
-          <button>Cart</button>
+          <IoCartOutline className="md:text-5xl" />
         </div>
       </div>
     </>
