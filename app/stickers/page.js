a4b943a6abc8ff8fdcdb2a3b9e0f54d034bdba5f
imt-eiaz <imteiaz@gmail.com>
2025-04-02 12:14:07 +0100

    39 to start

--- app/stickers/page.js
+++ app/stickers/page.js
@@ -1,7 +1,375 @@
 import React from "react";
+import Link from "next/link";
+
 
 const page = () => {
-  return <div>stickers</div>;
+  return <div>
+<div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+            <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+            <Link href={'/products/wear-the-code'}>
+              <a className="block relative rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className=" m-auto h-[30vh] md:mx-0 md:h-[36vh] block"
+                  src="https://m.media-amazon.com/images/I/61M9ZlENVzL._AC_SL1500_.jpg"
+                />
+              </a>
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  T-Shirts
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  Wear The Code
+                </h2>
+                <p className="mt-1">£16.00</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+              </Link>
+            </div>
+            
+
+           
+          </div>
+        </div>
+      </section>
+    </div>
+
+  </div>;
 };
 
 export default page;
