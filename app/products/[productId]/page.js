30c07f110a5d3a904a2559cc6dd635692c18cf97
imt-eiaz <imteiaz@gmail.com>
2025-04-03 17:44:33 +0100

    41 Start 1 min

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,6 +1,7 @@
 // function generateStaticParams() {}
 import Image from "next/image";
 
+
 // export default function ProductDetails({ params }) {
 // export default function ProductDetails() {
 const page = () => {
@@ -10,13 +11,16 @@ const page = () => {
         {/* <h1>Details about product {params.productId}</h1>; */}
 
         <section className="text-gray-600 body-font overflow-hidden">
+        
           <div className="container px-5 py-24 mx-auto">
+          
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
-              <Image
-                alt="ecommerce"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src="https://dummyimage.com/400x400"
+            <Image
+                // alt="ecommerce"
+                // className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                src= {"/lamp.jpg"} width={265} height={500} alt=""
               />
+              
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                 <h2 className="text-sm title-font text-gray-500 tracking-widest">
                   BRAND NAME
@@ -24,14 +28,14 @@ const page = () => {
                 <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                   The Catcher in the Rye
                 </h1>
-                <div className="flex mb-4">
+                {/* <div className="flex mb-4">
                   <span className="flex items-center">
                     <svg
                       fill="currentColor"
-                      // stroke="currentColor"
-                      // stroke-linecap="round"
-                      // stroke-linejoin="round"
-                      // stroke-width="2"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
                       className="w-4 h-4 text-pink-500"
                       viewBox="0 0 24 24"
                     >
@@ -121,7 +125,7 @@ const page = () => {
                       </svg>
                     </a>
                   </span>
-                </div>
+                </div> */}
                 <p className="leading-relaxed">
                   Fam locavore kickstarter distillery. Mixtape chillwave tumeric
                   sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
@@ -166,8 +170,8 @@ const page = () => {
                   <span className="title-font font-medium text-2xl text-gray-900">
                     $58.00
                   </span>
-                  <button className="flex ml-auto text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
-                    Button
+                  <button className="cursor-pointer flex ml-14 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
+                    Add to Cart
                   </button>
                   <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                     <svg
