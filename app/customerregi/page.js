deb66a6e9d82fd5c64f101d5714c31a2d29068f3
imt-eiaz <imteiaz@gmail.com>
2025-04-13 11:12:44 +0100

    after merge in pc

--- app/customerregi/page.js
+++ app/customerregi/page.js
@@ -12,8 +12,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { red } from "@mui/material/colors";
-import EmployeeForm from "@/src/pages/Employees/EmployeeForm";
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -33,11 +31,11 @@ const page = () => {
     <>
       <div class="flex flex-col text-center w-full mb-12">
         {/* <h1 class="sm:text-3xl text-2xl font-medium title-font mt-5 mb-4 text-gray-900"> */}
-        <Typography variant="h5" marginTop="20px" bgcolor="red" color="white">
+        <Typography variant="h5" marginTop="20px">
           PhoneBox Customer Regi
         </Typography>
         {/* </h1> */}
-        <EmployeeForm />
+
         {/* <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -67,6 +65,8 @@ const page = () => {
             </TableBody>
           </Table>
         </TableContainer> */}
+
+        <Employee />
       </div>
     </>
   );
